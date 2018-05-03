e95f591dc928da5f42eaad4662f0029221842f01
Hateman31 <metilogrunge31@gmail.com>
2018-05-03 07:06:49 +0300

    add newTask component

---
+++ client/src/components/newTask.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div( class="task")
+    label Name
+    input(v-model="name")
+    label Description
+    input(v-model="description")
+    button(@click="save") Save
+</template>
+
+<script>
+export default {
+  name: 'newTask',
+  props:{
+  },
+  data () {
+    return {
+		name:''
+		,description:''
+    }
+  },
+  methods:{
+	save(){
+		//~ this.$emit("saveTask")
+		this.$emit("saveTask",{
+			name: this.name,
+			description:this.description
+		})
+		//~ console.log('haha')
+	}  
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div{
+	display: grid
+}
+</style>
